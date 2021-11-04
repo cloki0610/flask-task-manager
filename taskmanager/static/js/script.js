d5927a82363ea42ca81d903cd6b7e60c998ea457
Rocky.C <cloki0610@gmail.com>
2021-11-04 00:22:17 +0000

    Deleting Categories(with model)

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+    let elems = document.querySelectorAll('.modal');
+    let instances = M.Modal.init(elems);
 });
