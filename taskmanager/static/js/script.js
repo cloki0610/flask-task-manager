8fd109dc67fa0207f92f333f7eb17f7e0308413b
Rocky.C <cloki0610@gmail.com>
2021-11-04 02:21:31 +0000

    Adding Tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,6 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll(".sidenav");
     M.Sidenav.init(sidenav);
+    // modal initialization
     let elems = document.querySelectorAll('.modal');
     let instances = M.Modal.init(elems);
+    // datepicker initialization
+    let datepicker = document.querySelectorAll(".datepicker");
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
 });
